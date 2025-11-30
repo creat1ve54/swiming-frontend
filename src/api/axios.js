08bccd90fca0ec686e8059b889c02027b086ef58
arthur <artyr.petrosyan15@mail.ru>
2025-11-30 16:06:01 +0300

    local

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://212.67.9.16:8000/api",
+  baseURL: "http://localhost:8000/api",
 });
 
 export const sportsmansAPI = {
